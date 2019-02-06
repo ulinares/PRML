047d8f4bda521056b40a5b30f68cdfd6d583d640
Uziel Linares <uzielnmtz@gmail.com>
2019-02-05 18:49:33 -0600

    fix dimensions mismatch problems, and add predict method

--- src/linear_regression.jl
+++ src/linear_regression.jl
@@ -1,3 +1,4 @@
+module Temp
 using LinearAlgebra
 # A simple linear regression
 
@@ -14,17 +15,19 @@ end
 LinearRegression(; eltype=Float64, λ=0.) = LinearRegression{Array{eltype, 2}}(λ)
 
 function fit!(lm::LinearRegression, Φ::S, y::T) where {S, T}
+    y = reshape(y, :, 1)
+    N, m = size(Φ)
     if lm.λ == 0
-        N, m = size(Φ)
         w_ml = (Φ' * Φ)^-1 * Φ'* y
         lm.coefs = Array{Float64, 2}(w_ml)
-        lm.σ = sqrt(sum([(y[i] - (lm.coefs' * Φ[i, :])[1])^2 for i in 1:N]) / N)
     else
-        id_size = size(Φ)[2]
-        I = Matrix{Float64}(I, id_size, id_size)
-        w_ml = (λ .* I + Φ' * Φ)^-1 + Φ'*y
-        lm.coefs = Array{Float64, 1}([w_ml])
+        λ = lm.λ
+        Id = Matrix{Float64}(I, m, m)
+        w_ml = (λ .* Id + Φ' * Φ)^-1 * Φ'*y
+        lm.coefs = Array{Float64, 2}(w_ml)
     end
+    lm.σ = sqrt(sum([(y[i] - (lm.coefs' * Φ[i, :])[1])^2 for i in 1:N]) / N)
+    nothing
 end
 
 # TODO: implement online learning via the partial_fit function
@@ -32,10 +35,12 @@ function partial_fit!(lm::LinearRegression)
 end
 
 function predict(lm::LinearRegression, X::T) where T <: Array
-    y_preds = Float64[]
-    for x in X
-        y = lm.coefs * x
+    y_preds = []
+    for i in 1:size(X)[1]
+        y = dot(X[i:i, :], lm.coefs)
         push!(y_preds, y)
     end
     return y_preds
+end
+
 end
