93026b69d5a79a5ad91c8d830fcd57d4a53171cc
Uziel Linares <uzielnmtz@gmail.com>
2019-02-09 16:29:48 -0600

    add method to generate data by classes

--- src/data.jl
+++ src/data.jl
@@ -8,3 +8,21 @@ function make_sine(N::Int; μ::Float64=0., σ::Float64=0.3)
     y = [sin(2π*x) + rand(d) for x in X]
     return X, y
 end
+
+# Generate data by clases with same covariance matrix
+# TODO: generate with different covariance?
+function make_classes(N::Int; n_class::Int=2, σ²=0.25)
+    X = []
+    T = []
+    μs = [rand(2) .* 5 .- 2.5 for _ in 1:n_class]
+    d = [MvNormal(μ, [σ², σ²]) for μ in μs]
+    for i in 1:n_class
+        for _ in 1:N
+            x = rand(d[i])
+            t = zeros(n_class); t[i] = 1
+            push!(X, x)
+            push!(T, t)
+        end
+    end
+    return X, T
+end
