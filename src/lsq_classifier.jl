6e4e6b87fdc472ac5f123d7148271a0efe23129b
Uziel Linares <uzielnmtz@gmail.com>
2019-02-12 15:52:05 -0600

    add predict method

--- src/lsq_classifier.jl
+++ src/lsq_classifier.jl
@@ -13,6 +13,14 @@ function fit!(lsq_clf::LsqClassifier, X̃::Matrix, T::Vector)
     fit!(lsq_clf, X̃, T)
     nothing
 end
-    
 
+function predict(lsq_clf::LsqClassifier, X̃::Matrix)
+    preds = [argmax(lsq_clf.W̃' * X[i, :]) for i in 1:size(X̃)[1]]
+    preds
+end
+
+# TODO: add plot_decision function
+function plot_decision()
+
+end
 end
