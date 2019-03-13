172256ba87bd45d0f4a5a85b276ebead31b7dade
Uziel Linares <uzielnmtz@gmail.com>
2019-03-13 13:45:54 -0600

    add computed quantities to fisher structure

--- src/fisher_discriminant.jl
+++ src/fisher_discriminant.jl
@@ -1,5 +1,10 @@
 mutable struct FisherDiscriminant
     w::Array
+    _m1::Array
+    _m2::Array
+    _S1::Array
+    _S2::Array
+    _S::Array
     FisherDiscriminant() = new()
 end
 
@@ -8,23 +13,23 @@ end
 function fit!(fd::FisherDiscriminant, X::Array, T::Vector)
     C₁ = X[T .== 1, :]
     C₂ = X[T .== 2, :]
-    m₁ = mean(C₁, dims=1)'
-    m₂ = mean(C₂, dims=1)'
-    S₁ = zeros(2, 2); S₂ = zeros(2, 2)
+    fd._m1 = mean(C₁, dims=1)'
+    fd._m2 = mean(C₂, dims=1)'
+    fd._S1 = zeros(2, 2); fd._S2 = zeros(2, 2)
 
     for i in 1:size(C₁)[1]
         xₙ = X[i, :]
-        S₁ += (xₙ - m₁) * (xₙ - m₁)'
+        fd._S1 += (xₙ - fd._m1) * (xₙ - fd._m1)'
     end
 
     for i in 1:size(C₂)[1]
         xₙ = X[i, :]
-        S₂ += (xₙ - m₂) * (xₙ - m₂)'
+        fd._S2 += (xₙ - fd._m2) * (xₙ - fd._m2)'
     end
 
-    S = S₁ + S₂
+    fd._S = fd._S1 + fd._S2
 
-    fd.w = S^-1 * (m₂ - m₁)
+    fd.w = fd._S^-1 * (fd._m2 - fd._m1)
     nothing
 end
 
